c778da7e19eb3c49dd0025bc2b024e871f267391
Mikael Peltomaa <contact@mikaelpeltomaa.fi>
2025-01-26 16:57:23 +0200

    Added Anna Palautetta button to footer

--- frontend/src/routes/Root.tsx
+++ frontend/src/routes/Root.tsx
@@ -1,16 +1,18 @@
 import { Link, Outlet } from "react-router-dom";
+import Footer from "../footer"; // Import the Footer component
 
 export function Root() {
   return (
-    <>
+    <div className="flex flex-col min-h-screen">
       <header className="bg-blue-700 p-4 text-xl flex">
         <div className="container mx-auto">
           <Link to="/">LiveLoki</Link>
         </div>
       </header>
-      <div className="overflow-y-auto inline-block grow px-2">
-        <Outlet />
-      </div>
-    </>
+      <main className="overflow-y-auto grow px-2">
+        <Outlet /> {/* This renders child components */}
+      </main>
+      <Footer /> {/* Add Footer here */}
+    </div>
   );
 }

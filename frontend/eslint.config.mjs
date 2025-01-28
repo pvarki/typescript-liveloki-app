19f3d667311fe0017f5a374ac2ad212976759e95
Aarni Koskela <akx@iki.fi>
2025-01-28 13:23:37 +0200

    Wire up eslint-plugin-unicorn

--- frontend/eslint.config.mjs
+++ frontend/eslint.config.mjs
@@ -2,6 +2,7 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 import reactPlugin from "eslint-plugin-react";
+import eslintPluginUnicorn from "eslint-plugin-unicorn";
 
 export default [
   { files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"] },
@@ -12,6 +13,15 @@ export default [
   ...tseslint.configs.recommended,
   reactPlugin.configs.flat.recommended,
   reactPlugin.configs.flat["jsx-runtime"],
+  eslintPluginUnicorn.configs["flat/recommended"],
+  {
+    rules: {
+      "unicorn/filename-case": "off",
+      "unicorn/no-array-callback-reference": "off", // TypeScript deals with this
+      "unicorn/no-null": "off",
+      "unicorn/prevent-abbreviations": "off",
+    },
+  },
   {
     languageOptions: {
       ...reactPlugin.configs.flat.recommended.languageOptions,

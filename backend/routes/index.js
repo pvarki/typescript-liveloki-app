4f0d68645774bb431d051abdec6a61eefc06e712
Sami J. Lehtinen <sjl@iki.fi>
2025-04-22 20:17:39 +0300

    Add /api/v1 routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -7,4 +7,7 @@ const router = express.Router();
 router.use('/api', eventRoutes);
 router.use('/rmapi', rmRoutes);
 
+router.use('/api/v1', eventRoutes);
+router.use('/api/v1', rmRoutes);
+
 export default router;

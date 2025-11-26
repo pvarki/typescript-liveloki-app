97011ce76d7c386238d05334e8fcd45554470f8c
3zk1m0 <sander.kallio@gmail.com>
2025-11-26 14:09:42 +0200

    Added description endpoint

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import eventRoutes from './eventRoutes.js';
 import rmRoutes from './rmRoutes.js';
 import userRoutes from './userRoutes.js';
+import descriptionRoutes from './descriptionRoutes.js';
 
 const router = express.Router();
 
@@ -13,5 +14,8 @@ router.use('/api/v1', rmRoutes);
 
 router.use('/api/users', userRoutes);
 router.use('/api/v1/users', userRoutes);
+
+router.use('/api/description', descriptionRoutes);
+router.use('/api/v1/description', descriptionRoutes);
 
 export default router;
